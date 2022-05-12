467eba86d5bc12d63700445e74468322bae0a009
Andrei K <andrei@12ak.com>
2022-05-12 18:56:06 -0400

    Get input

--- main.go
+++ main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world.")
+	var path string
+	fmt.Println("Enter the path to the folder you want to search and delete files in:")
+	fmt.Scan(&path)
+
+	var regex string
+	fmt.Println("Enter the string that will be used to search and delete files:")
+	fmt.Scan(&regex)
+
+	fmt.Printf("\nPath: %s\nRegex: %s\n", path, regex)
+
 }
